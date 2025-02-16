de221762abfbe8279d14977d654c7e0255fc39a5
Kevin <kevinhowardrc@gmail.com>
2025-02-17 01:23:12 +0800

    Update page.tsx

--- app/u/[uid]/page.tsx
+++ app/u/[uid]/page.tsx
@@ -1,6 +1,6 @@
 import Pageclient from "./page.client";
 
-export default async function page({ params }: { params: { uid: string } }) {
-  const { uid } = await params;
-  return <Pageclient uid={uid}/>
+export default async function Page({ params }: { params: Promise<{ uid: string }> }) {
+  const uid = (await params).uid;
+  return <Pageclient uid={uid} />;
 }
